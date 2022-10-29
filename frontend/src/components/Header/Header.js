d89decbc9e1c3ac609c84afc0c37a5f4d1b3963b
nikitamastinen <nikmastinen@gmail.com>
2022-10-29 19:51:28 +0300

    added Header content

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -2,6 +2,10 @@ import { useTheme } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import {Button} from "@mui/material";
+import icon from '../../assets/upload.svg';
+import {grey} from '@mui/material/colors';
+
 
 // TODO - сделать по дизайну
 export const Header = () => {
@@ -16,17 +20,55 @@ export const Header = () => {
              background: 'none',
              color: '#9EA2B0',
              width: `calc(100% - 96px)`,
-             height: '80px',
+             height: '90px',
          }}>
               <Toolbar sx={{
                   width: '100%',
                   marginLeft: '20px',
                   background: theme.palette.background.default,
                   borderRadius: '20px',
+                  justifyContent: 'space-between',
+                  marginRight: '16px',
+                  marginTop: '8px',
               }}>
-                   <Typography variant="h6" noWrap component="div">
-                       Mini variant drawer
-                   </Typography>
+                   {/*<Typography variant="h6" noWrap component="div">*/}
+                   {/*    Mini variant drawer*/}
+                   {/*</Typography>*/}
+                      <div>
+                          <Typography variant={'h5'} style={{
+                              color: theme.palette.text.primary,
+                          }}>
+                              2 323 232
+                          </Typography>
+                          <Typography variant={'subtitle1'} style={{
+                              color: theme.palette.text.secondary,
+                          }}>
+                              составит охват населения
+                          </Typography>
+                      </div>
+                      <div style={{
+                          display: 'flex',
+                          flexDirection: 'row',
+                      }}>
+                          <Button style={{borderRadius: '12px', padding: '16px 32px', backgroundColor: theme.palette.action.selected, boxShadow: 'none'}} variant="contained">
+                              <img src={icon}/>
+                              <div style={{padding: '8px'}}></div>
+                              <Typography variant={'h6'} style={{color: theme.palette.text.secondary}}>
+                                  XSLX
+                              </Typography>
+                          </Button>
+
+                          <div style={{marginLeft: '8px'}}></div>
+
+                          <Button style={{borderRadius: '12px', padding: '16px 32px', backgroundColor: theme.palette.action.selected, boxShadow: 'none'}} variant="contained">
+                              <img src={icon}/>
+                              <div style={{padding: '8px'}}></div>
+                              <Typography variant={'h6'} style={{color: theme.palette.text.secondary}}>
+                                  PDF
+                              </Typography>
+                          </Button>
+                      </div>
+                  {/*</div>*/}
               </Toolbar>
          </Box>
 )}
