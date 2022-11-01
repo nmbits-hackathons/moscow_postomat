969084938f819299b4aa45c217bb3e15d070f3c0
Ivan Sosnovich <isosnovich@yandex.ru>
2022-11-01 22:34:32 +0300

    получение постаматов

--- frontend/src/api/map/index.js
+++ frontend/src/api/map/index.js
@@ -1,5 +1,30 @@
 import instance from "../axios";
 
-export const getPostamats = () => {
-   return instance.get('/get_postamats/')
+const mockFilters = {
+   "places": {
+      "auto": false,
+      "kiosk": 0,
+      "sports": 0,
+      "library": 0,
+      "culture_house": 0,
+      "multifunctional": 0,
+      "underground": 0,
+      "house": 0
+   },
+   "placement": [
+      {
+         "district": "string",
+         "region": "string"
+      }
+   ],
+   "model_keyword": "k-means",
+   "sector": {
+      "active": false,
+      "coordinates": "string",
+      "radius": 0
+   }
+}
+
+export const getPostamats = (filters = mockFilters) => {
+   return instance.post('/get_postamats/', filters)
 }
--- frontend/src/pages/Maps/util/getPostamats/getPostamats.js
+++ frontend/src/pages/Maps/util/getPostamats/getPostamats.js
@@ -1,6 +1,25 @@
 import pick from 'lodash/pick'
 import get from 'lodash/get'
-export const getPostamatsForData = (data) => {
-    return data
-        .map(el => ({ ...pick(el, ['id', 'title', 'description']), ...pick(get(el, 'placement'), ['coordinates', 'address_string'])}))
+export const getPostamatsForData = (points) => {
+    return points
+        .map(el => ({ ...pick(el, ['id', 'title', 'description', 'indicator', 'square']), ...pick(get(el, 'placement'), ['coordinates', 'address_string'])}))
 }
+
+// {
+//     "id": 1,
+//     "title": "test",
+//     "description": "str - optional",
+//     "type": "theatre",
+//     "indicator": 9.3,
+//     "placement": {
+//     "region": "test",
+//         "district": "test",
+//         "coordinates": "55.834472,37.65805",
+//         "address_string": "пр-кт. Мира, д. 188 Б, к. 1, Москва"
+// },
+//     "square": 54804.1,
+//     "year": 2014,
+//     "number of floors": 58,
+//     "number of entrances": 1,
+//     "number_of_apartments": 370
+// },
