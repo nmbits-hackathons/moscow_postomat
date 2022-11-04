0f4952e6e61855b16598f1c34f506480412c9af0
destitutiones <destitutiones@yandex-team.ru>
2022-11-04 16:30:21 +0300

    Added stupid coverage creation

---
+++ backend/core/coverage.py
@@ -0,0 +1,16 @@
+import numpy as np 
+import pandas as pd
+import json
+
+
+def make_coverage(request):
+    '''
+    '''
+    if request['places']['library'] > 0:
+        df = pd.read_csv('./datasets/math_model_result.csv')
+        df = df.iloc[:, 1:]
+        a = np.random.choice(200, size=50, replace=False)
+        res = df.iloc[a, :].sort_values(by='Mark', ascending=False)
+        return res.to_json()
+    else:
+        return pd.DataFrame().to_json()
